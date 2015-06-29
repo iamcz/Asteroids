75f7f0ead1b11a72cb540ba2c6fc19e5efd90ec9
Charles A Zahn <CharlesAZahn@gmail.com>
2015-06-29 13:46:05 -0400

    Add Game constructor.

---
+++ asteroids/lib/game.js
@@ -0,0 +1,23 @@
+(function () {
+  if (typeof window.Asteroids === "undefined") {
+    window.Asteroids = {};
+  }
+
+  var Game = Asteroids.Game = function () {
+    this.asteroids = [];
+  };
+
+  Game.NUM_ASTEROIDS = 3;
+  Game.DIM_X = 300;
+  Game.DIM_Y = 300;
+
+  Game.prototype.addAsteroid = function () {
+    this.asteroids.push( new Asteroids.Asteroid({ pos: Game.randomPos() }));
+  }
+
+  Game.randomPos = function () {
+    var x = Math.random() * Game.DIM_X;
+    var y = Math.random() * Game.DIM_Y;
+    return [x, y];
+  };
+})();
