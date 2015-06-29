83c7d6f52d6900aa9e064b506218cdf4a61ee157
Charles A Zahn <CharlesAZahn@gmail.com>
2015-06-29 11:32:42 -0400

    Finish curry exercises.

--- arguments-exercises.js
+++ arguments-exercises.js
@@ -1,5 +1,5 @@
 var sum = function() {
-  args = Array.prototype.slice.call(arguments);
+  var args = Array.prototype.slice.call(arguments);
 
   var total = 0;
   args.forEach( function (el) {
@@ -10,7 +10,7 @@ var sum = function() {
 };
 
 Function.prototype.myBind = function(context) {
-  args = Array.prototype.slice.call(arguments, 1);
+  var args = Array.prototype.slice.call(arguments, 1);
   var fn = this;
 
   return function () {
@@ -32,9 +32,53 @@ var meow = function (meow, anotherMeow) {
 var c = new Cat("whiskers");
 var catMeow = meow.myBind(c, "meow");
 
-var catMeow = function () {
-  var innerArgs = Array.prototype.slice.call(arguments)
-  return fn.apply(context, ["meow].concat(innerArgs));
-};
+// var catMeow = function () {
+//   var innerArgs = Array.prototype.slice.call(arguments)
+//   return fn.apply(c, ["meow"].concat(innerArgs));
+// };
 
 catMeow("anotherMeow");
+
+var curriedSum = function (numArgs) {
+
+  var numbers = [];
+
+  var _curriedSum = function (num) {
+    numbers.push(num);
+    if (numbers.length === numArgs) {
+      var total = 0;
+      numbers.forEach(function (el) {
+        total += el;
+      })
+      return total;
+    } else {
+      return _curriedSum;
+    };
+  };
+
+  return _curriedSum;
+};
+
+curriedSum(2)(1)(1);
+
+Function.prototype.curry = function (numArgs) {
+  var fn = this;
+  var args = [];
+
+  var _curriedFunc = function (arg) {
+    args.push(arg);
+    if (args.length === numArgs) {
+      return fn.apply(null, args);
+    } else {
+      return _curriedFunc;
+    };
+  };
+
+  return _curriedFunc;
+};
+
+function sumThree(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+
+sumThree.curry(3)(4)(20)(6); // == 30
